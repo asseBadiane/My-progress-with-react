1f2601e8ab6b83fcf16869a7c3d74c67d884bda8
ReddingCode <badianeasse99@gmail.com>
2023-03-02 15:29:55 +0000

    Les links soient dinamiques

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,9 +4,9 @@ export default function Menu() {
     return (
         <div className="menu">
             <ul>
-                <li><NavLink to="/">Home </NavLink></li>
-                <li><NavLink to="/add">Add Techno</NavLink></li>
-                <li><NavLink to="/list">All Technos</NavLink></li>
+                <li><NavLink to="/" className={({ isActive }) => isActive ? "activeLink" : undefined}>Home </NavLink></li>
+                <li><NavLink to="/add" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Add Techno</NavLink></li>
+                <li><NavLink to="/list" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>All Technos</NavLink></li>
             </ul>
         </div>
     )
