35abd6fae66307968de9aa5d2218afdfc08de4f1
ReddingCode <badianeasse99@gmail.com>
2023-03-02 11:06:07 +0000

    Mon premiere commit, le structure du projet

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import '../css/app.css';
+import Home from './Home';
+
+function App() {
+  return (
+    < Home />
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>My Progresssion</h1>
+            <div>welcome to this site devoted to your programming progression</div>
+
+        </div>
+    )
+}
