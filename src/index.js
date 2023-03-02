c571763930d49845a00ebba269f0fb053fa3b4a3
ReddingCode <badianeasse99@gmail.com>
2023-03-02 14:10:02 +0000

    J'ai ajouter les routers , j'ai aussi installer 'react-router-dom'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import '../src/css/app.css';
 import App from './pages/App';
 // import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,10 @@ import App from './pages/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter> 
     <App />
+
+    </BrowserRouter>
   </React.StrictMode>
 );
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import Menu from '../components/Menu';
 import '../css/app.css';
 import Home from './Home';
@@ -8,10 +9,16 @@ import TechnoList from './TechnoList';
 function App() {
   return (
     <>
-      <Menu />
+    <Menu />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/add' element={<TechnoAdd />} />
+      <Route path='/list' element={<TechnoList />} />
+    </Routes>
+      {/* <Menu />
       <Home />
       <TechnoAdd />
-      <TechnoList />
+      <TechnoList /> */}
     </>
   );
 }
