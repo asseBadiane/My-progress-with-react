d0baa53b1419d408e9cb2f11d4a3fbbb581cd58e
ReddingCode <badianeasse99@gmail.com>
2023-03-02 14:18:43 +0000

    fix bugs

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,7 @@ export default function Menu() {
     return (
         <div className="menu">
             <ul>
-                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/">Home </NavLink></li>
                 <li><NavLink to="/add">Add Techno</NavLink></li>
                 <li><NavLink to="/list">All Technos</NavLink></li>
             </ul>
